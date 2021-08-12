f14cb87733f1324265d5c3c88d1a35274d1a4411
Goodness Ezeokafor <gootech442@gmail.com>
2021-08-12 12:47:51 +0100

    added gorotines

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/GoodnessEzeokafor/small-go-projects/nhlApi"
@@ -12,7 +13,7 @@ import (
 func main() {
 	// help benchmarking the request time
 	now := time.Now()
-	rosterFile, err := os.OpenFile("rosters.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	rosterFile, err := os.OpenFile("rosters.txt", os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		log.Fatalf("error opening the file rosters.txt: %v", err)
 	}
@@ -24,11 +25,42 @@ func main() {
 	if err != nil {
 		log.Fatalf("error opening the file rosters.txt: %v", err)
 	}
+	var wg sync.WaitGroup
+	wg.Add(len(teams))
+
+	// unbuffered channel
+	results := make(chan []nhlApi.Roster)
+
 	for _, team := range teams {
-		log.Printf("----------")
-		log.Printf("Name %s", team.Name)
-		log.Printf("----------")
+		go func(team nhlApi.Team) {
+			roster, err := nhlApi.GetRoster(team.ID)
+			if err != nil {
+				log.Fatalf("error getting roster %v", err)
+			}
+			results <- roster
+			wg.Done()
+		}(team)
 
 	}
+	go func() {
+		wg.Wait()
+		// close channel
+		close(results)
+	}()
+	display(results)
 	log.Printf("took %v", time.Now().Sub(now).String())
+}
+
+func display(results chan []nhlApi.Roster) {
+	for r := range results {
+		for _, ros := range r {
+			log.Printf("----------")
+			log.Printf("ID: %d\n", ros.Person.ID)
+			log.Printf("Name: %s\n", ros.Person.FullName)
+			log.Printf("Position: %s\n", ros.Position.Abbreviation)
+			log.Printf("Jersey: %s\n", ros.JerseyNumber)
+
+			log.Printf("----------")
+		}
+	}
 }
